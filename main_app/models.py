72739450d84cfcf625fc821c05827a0ce4f393a4
Rashel <rashel.gandhi@gmail.com>
2020-09-25 12:21:38 -0600

    Complete Step 3.2

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Widget(models.Model):
+    description = models.CharField(max_length=50)
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return self.item
+
+    def get_absolute_url(self):
+        return reverse('index')
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Widget
 
 # Create your views here.
+def index(request):
+    widgets = Widget.objects.all()
+    return render(request, 'index.html', {'widgets': widgets})
